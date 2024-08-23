f4d6ca3e30ebdc681c90ef0136ba83242c224e03
PPLong <longgordon1201@gmail.com>
2024-08-23 16:01:36 +0700

    modify src

--- src/app/code/Custom/CustomModule/view/frontend/requirejs-config.js
+++ src/app/code/Custom/CustomModule/view/frontend/requirejs-config.js
@@ -1,6 +1,6 @@
 var config = {
     paths: {
-        simple_weather: "Custom_CustomModule/js/jquery.simpleWeather.min.js",
+        simple_weather: "Custom_CustomModule/js/jquery.simpleWeather.min",
     },
     shim: {
         simple_weather: {
