0255e91e24818050492a78877e5437b8d89e3955
PPLong <longgordon1201@gmail.com>
2024-08-28 16:11:59 +0700

    add model

--- src/app/code/Custom/CustomModule/Controller/Index/Index.php
+++ src/app/code/Custom/CustomModule/Controller/Index/Index.php
@@ -3,27 +3,48 @@
 namespace Custom\CustomModule\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
-use Magento\Framework\View\Result\PageFactory;
+use Custom\CustomModule\Model\CustomTableFactory;
+use Magento\Framework\App\Action\Context;
+use \Custom\CustomModule\Model\CustomTable;
 
 class Index extends Action
 {
+    protected $_customTableFactory;
+
+    public function __construct(Context $context, CustomTableFactory $customTableFactory)
+    {
+        $this->_customTableFactory = $customTableFactory;
+        parent::__construct($context);
+    }
 
     public function execute()
     {
-        $collection = $this->_objectManager->create('Custom\CustomModule\Model\ResourceModel\CustomTable\Collection');
+        $connection = $this->_objectManager->get('Magento\Framework\App\ResourceConnection')->getConnection();
+        $sql = "SELECT * FROM custom_table WHERE name_custom = :name_custom";
+        $binds = ['name_custom' => 'long'];
+        $data = $connection->fetchAll($sql);
 
-        $collection->addFieldToFilter('name_custom', 'test');
+        print_r($data);
+        exit;
+
+        $model = $this->_customTableFactory->create();
+        $collection = $model->getCollection();
+        $data = $collection->getData();
+        print_r($data);
+        exit;
+
+        $model = $this->_objectManager->create(CustomTable::class);
+        $data1 = $model->getData();
+
+        $collection = $model->getCollection();
+
+        $collection->addFieldToFilter('name_custom', 'long');
 
         foreach ($collection as $item) {
-            $item->setNameCustom('test1');
+            $item->setNameCustom('long');
             $item->save();
         }
 
-        $data1 = $collection->getData();
-
-        echo '<pre>';
-        print_r($data1);
-        echo '</pre>';
         echo 'Data updated and saved successfully';
         exit;
     }
