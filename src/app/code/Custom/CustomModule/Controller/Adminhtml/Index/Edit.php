ace69c83db335200bed02b78803cbd92c801f780
PPLong <longgordon1201@gmail.com>
2024-09-05 15:59:18 +0700

    create new page

---
+++ src/app/code/Custom/CustomModule/Controller/Adminhtml/Index/Edit.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Custom\CustomModule\Controller\Adminhtml\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Backend\App\Action;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Backend\App\Action\Context;
+
+/**
+ * Edit CMS page action.
+ */
+class Edit extends \Magento\Backend\App\Action implements HttpGetActionInterface
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Custom_CustomModule::custom_table_save';
+
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry;
+
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \Magento\Framework\Registry $registry
+     */
+    public function __construct(Context $context, PageFactory $resultPageFactory, Registry $registry)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->_coreRegistry = $registry;
+        parent::__construct($context);
+    }
+
+    /**
+     * Init actions
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    protected function _initAction()
+    {
+        // load layout, set active menu and breadcrumbs
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Custom_CustomModule::custom_table');
+        return $resultPage;
+    }
+
+    /**
+     * Edit CMS page
+     *
+     * @return \Magento\Backend\Model\View\Result\Page|\Magento\Backend\Model\View\Result\Redirect
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function execute()
+    {
+        // echo "edit action";
+        // 1. Get ID and create model
+        $id = $this->getRequest()->getParam('id_custom');
+        $model = $this->_objectManager->create(\Custom\CustomModule\Model\CustomTable::class);
+        $model->load($id);
+
+        // 2. Initial checking
+        if ($id) {
+            $model->load($id);
+            if (!$model->getId()) {
+                $this->messageManager->addErrorMessage(__('This page no longer exists.'));
+                /** \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+                $resultRedirect = $this->resultRedirectFactory->create();
+                return $resultRedirect->setPath('*/*/');
+            }
+        }
+
+        $this->_coreRegistry->register('custom_table', $model);
+
+        // 5. Build edit form
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->_initAction();
+        // $resultPage->getConfig()->getTitle()->prepend(__('Pages'));
+        $resultPage->getConfig()->getTitle()
+            ->prepend($model->getId() ? $model->getNameCustom() : __('New Custom Table'));
+
+        return $resultPage;
+    }
+}
---
+++ src/app/code/Custom/CustomModule/Controller/Adminhtml/Index/NewAction.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Custom\CustomModule\Controller\Adminhtml\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Backend\Model\View\Result\ForwardFactory;
+use Magento\Backend\App\Action\Context;
+
+/**
+ * Create CMS page action.
+ */
+class NewAction extends \Magento\Backend\App\Action implements HttpGetActionInterface
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Custom_CustomModule::custom_table_save';
+
+    /**
+     * @var \Magento\Backend\Model\View\Result\Forward
+     */
+    protected $resultForwardFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+     */
+    public function __construct(Context $context, ForwardFactory $resultForwardFactory)
+    {
+        $this->resultForwardFactory = $resultForwardFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Forward to edit
+     *
+     * @return \Magento\Backend\Model\View\Result\Forward
+     */
+    public function execute()
+    {
+        // echo "new action";
+        /** @var \Magento\Backend\Model\View\Result\Forward $resultForward */
+        $resultForward = $this->resultForwardFactory->create();
+        return $resultForward->forward('edit');
+    }
+}
